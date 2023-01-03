03ca2ae9ed127581da90099730b4b4c0a979d8a0
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-01-03 14:03:11 +0200

    Update better_ping.c

--- better_ping.c
+++ better_ping.c
@@ -24,14 +24,16 @@
 #define ICMP_HDRLEN 8
 
 unsigned short checksum(void *b, int len)
-{	unsigned short *buf = b;
+{	
+    unsigned short *buf = b;
 	unsigned int sum=0;
-	unsigned short result;
+	unsigned short result = 0;
 
 	for ( sum = 0; len > 1; len -= 2 )
 		sum += *buf++;
 	if ( len == 1 )
 		sum += *(unsigned char*)buf;
+
 	sum = (sum >> 16) + (sum & 0xFFFF);
 	sum += (sum >> 16);
 	result = ~sum;
@@ -62,8 +64,8 @@ int main(int argc, char *argv[])
     struct iphdr *ip;
     memset(&ip , 0 , sizeof(ip));
 
-    char sendandrecv[IP_MAXPACKET] = {'\0'};
-    size_t buffer_len = sizeof(sendandrecv);
+    char recvd[IP_MAXPACKET] = {'\0'};
+    size_t recvd_len = sizeof(recvd);
     
     char packet[IP_MAXPACKET] = {0} , data[MSG_LEN] = {0};
     size_t data_len = strlen(data) + 1;
@@ -177,8 +179,8 @@ int main(int argc, char *argv[])
             memset(buffer, '\0', BUFSIZ);
 
         addr_len = sizeof(addr_ping); // receiving ICMP-ECHO-REPLEY
-        bzero(sendandrecv, IP_MAXPACKET);
-        while ((len = recvfrom(sock, sendandrecv, buffer_len, 0, (struct sockaddr *)&addr_ping, &addr_len)))
+        bzero(recvd, IP_MAXPACKET);
+        while ((len = recvfrom(sock, recvd, recvd_len, 0, (struct sockaddr *)&addr_ping, &addr_len)))
         {
             if (len == -1)
             {
@@ -215,11 +217,11 @@ int main(int argc, char *argv[])
             
             time -= 1000; // removing one second because of sleep(1)
 
-            ip = (struct iphdr*)sendandrecv;
+            ip = (struct iphdr*)recvd;
 
-            printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.2f ms\n", 
-                len, inet_ntoa(*(struct in_addr*)&ip->saddr), icmp.un.echo.sequence , ip->ttl, time);
-           
+            printf("%d bytes has been recv from %s to " , len, inet_ntoa(*(struct in_addr*)&ip->saddr));
+            printf("%s: icmp_seq=%d ttl=%d time=%.2f ms\n", inet_ntoa(*(struct in_addr*)&ip->daddr) , icmp.un.echo.sequence , ip->ttl, time);
+
             seq++;
 
             sleep(1);
